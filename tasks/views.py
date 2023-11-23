cdceedfb973900493fe8ba6798496d3ca8427e35
Grimnebuulen <mr.trader92@gmail.com>
2023-11-23 09:15:27 +0200

    fix

--- tasks/views.py
+++ tasks/views.py
@@ -25,12 +25,3 @@ class TaskModelViewSet(ModelViewSet):
             queryset = self.queryset.filter(owner=request.user)
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
-
-
-# class TaskListView(APIView):
-#     permission_classes = [permissions.IsAdminUser]
-    
-#     def get(self, request):
-#         tasks = Task.objects.all()
-#         serializer = TaskSerializer(tasks, many=True)
-#         return Response(serializer.data, status.HTTP_200_OK)
