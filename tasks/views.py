68e566a39649e0530a8aeed366678136e9c3da9a
Grimnebuulen <mr.trader92@gmail.com>
2023-11-28 13:43:27 +0200

    updated TaskModelViewSet

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
@@ -7,13 +8,20 @@ from .models import Task
 from users.permissions import IsOwner, IsOwnerOrAdmin
 
 
+User = get_user_model()
+
+
 class TaskModelViewSet(ModelViewSet):
     """
-    A ViewSet for handling Task model operations.
+    A ViewSet for managing Task model operations.
 
     Permissions:
     - `list`: Available to owners or admin users.
     - Other actions: Available to owners only.
+    
+    This view allows listing tasks based on the query parameters provided:
+    - `done`: Filters tasks by their completion status (e.g., done=true/false).
+    - `owner`: Filters tasks by owner (if the requesting user is a superuser).
     """
     
     queryset = Task.objects.all()
@@ -26,9 +34,20 @@ class TaskModelViewSet(ModelViewSet):
         return super().get_permissions()
     
     def list(self, request, *args, **kwargs):
+        done_param = request.query_params.get('done', None)
+        owner_param = request.query_params.get('owner', None)
+                
         if request.user.is_superuser:
             queryset = self.queryset
+            if owner_param is not None:
+                queryset = queryset.filter(owner=owner_param)
+            
         else:
             queryset = self.queryset.filter(owner=request.user)
+        
+        if done_param is not None:
+            done_param = str(done_param.capitalize())
+            queryset = queryset.filter(done=done_param)
+        
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
